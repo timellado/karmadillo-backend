17c81e395ae59928a08161aaa5bcc43b42ec697d
timellado <timellado@github.com>
2020-06-04 18:27:05 +0200

    Competition model

---
+++ src/models/competition.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+
+// Define the movie schema
+const CompetitionSchema  = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: String,
+    tag: {
+        type: String,
+        required: true
+    },
+    start_time: Date,
+    end_time: Date,
+    goal: String
+});
+
+CompetitionSchema.set('versionKey', false);
+CompetitionSchema.set('timestamps', true);
+
+
+// Export the Movie model
+module.exports = mongoose.model('Competition', CompetitionSchema);
