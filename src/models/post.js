91856ea3a25401ad93bdc7d45e0e05084d66eec1
Sagar Datta <s3605651@student.rmit.edu.au>
2020-06-04 19:08:20 +0200

    creation of post model

---
+++ src/models/post.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+
+// Define the movie schema
+const PostSchema  = new mongoose.Schema({
+    postPic: Image,
+    description: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    }
+});
+
+PostSchema.set('versionKey', false);
+PostSchema.set('timestamps', true);
+
+
+// Export the Movie model
+module.exports = mongoose.model('Post', PostSchema);
