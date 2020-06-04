37f6a3203d99e897eb5ce93945475ed205f3f03f
Sagar Datta <s3605651@student.rmit.edu.au>
2020-06-04 19:10:51 +0200

    edit post model to remove date variable

--- src/models/post.js
+++ src/models/post.js
@@ -9,10 +9,6 @@ const PostSchema  = new mongoose.Schema({
     description: {
         type: String,
         required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true
     }
 });
 
