5537d50a25bcb398a2023e917b4e033c5a9974b1
timellado <timellado@github.com>
2020-06-07 16:47:56 +0200

    added update and delete to post and competition

--- src/controllers/competition.js
+++ src/controllers/competition.js
@@ -38,6 +38,61 @@ const create = async (req, res) => {
     }
 };
 
+const read = async (req, res) => {
+    try {
+      let competition = await CompetitionModel.findById(req.params.id).exec();
+  
+      if (!competition) return res.status(404).json({
+        error: 'Not Found',
+        message: `competition not found`
+      });
+  
+      return res.status(200).json(competition)
+    } catch(err) {
+      return res.status(500).json({
+        error: 'Internal Server Error',
+        message: err.message
+      });
+    }
+  };
+  
+  const update = async (req, res) => {
+      if (Object.keys(req.body).length === 0) {
+          return res.status(400).json({
+              error: 'Bad Request',
+              message: 'The request body is empty'
+          });
+      }
+  
+      try {
+        let competition = await CompetitionModel.findByIdAndUpdate(req.params.id, req.body, {
+          new: true,
+          runValidators: true
+        }).exec();
+  
+        return res.status(200).json(competition);
+      } catch(err) {
+        return res.status(500).json({
+          error: 'Internal server error',
+          message: err.message
+        });
+      }
+  };
+  
+  const remove = async (req, res) => {
+    try {
+      await CompetitionModel.findByIdAndRemove(req.params.id).exec();
+  
+      return res.status(200).json({message: `competition with id${req.params.id} was deleted`});
+    } catch(err) {
+      return res.status(500).json({
+        error: 'Internal server error',
+        message: err.message
+      });
+    }
+  };
+  
+
 const list  = async (req, res) => {
     try {
       let competitions = await CompetitionModel.find({}).exec();
@@ -54,5 +109,8 @@ const list  = async (req, res) => {
 
 module.exports = {
     create,
+    read,
+    update,
+    remove,
     list
 };
