caa14b2a2de8dc9f3f00e33682a10e3a1e03535f
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2020-07-05 14:09:21 +0200

    added route to read a user by id

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,7 @@ const UserController = require('../controllers/user');
 
 router.get('/', UserController.list); // List all users
 // router.post('/', middlewares.checkAuthentication, UserController.create); // Create a new user
-// router.get('/:id', MovieController.read); // Read a movie by Id
+ router.get('/:id', UserController.read); // Read a user by Id
 // router.put('/:id', middlewares.checkAuthentication, MovieController.update); // Update a movie by Id
 // router.delete('/:id', middlewares.checkAuthentication, MovieController.remove); // Delete a movie by Id
 
