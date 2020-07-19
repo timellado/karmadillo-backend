7ddf2a887c5de4423a2316a05209f36c6a5048e8
timellado <timellado@uc.cl>
2020-07-19 23:37:47 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,17 +7,13 @@ const helmet     = require('helmet');
 const middlewares = require('./middlewares');
 
 const auth  = require('./routes/auth');
-<<<<<<< HEAD
 const movie  = require('./routes/movie');
 const user = require('./routes/user');
 const competition = require('./routes/competition');
 const comment = require('./routes/comment');
 const post = require('./routes/post');
- const activity = require('./routes/activity');
-=======
-const movie = require('./routes/movie');
+const activity = require('./routes/activity');
 
->>>>>>> master
 
 const api = express();
 
@@ -37,16 +33,11 @@ api.get('/', (req, res) => {
 
 // API routes
 api.use('/auth'  , auth);
-<<<<<<< HEAD
 api.use('/movies'  , movie);
 api.use('/posts', post);
 api.use('/users', user);
 api.use('/activities', activity);
 api.use('/comments', comment);
 api.use('/competitions', competition);
-=======
-api.use('/movies', movie);
-
->>>>>>> master
 
 module.exports = api;
